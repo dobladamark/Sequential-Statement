050217e0c134e8984bff4ab0a42a1a2462298961
dobladamark <122426238+dobladamark@users.noreply.github.com>
2024-11-24 15:12:14 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+    string name;
+    cout << "What's your Name? ";
+    cin >> name;
+
+
+
+    int age;
+    cout << "How old are you? ";
+    cin >> age;
+
+
+
+    string address;
+    cout << "Where you living?";
+    cin >> address;
+
+
+
+    cout << "My name is "<< name<<endl;
+    cout << "My age is "<<age<<endl;
+
+    cout << "I'm from "<<address<<endl;
+
+    return 0;
+}
+
